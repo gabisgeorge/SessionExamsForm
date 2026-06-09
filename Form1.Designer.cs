3aef69c3b548c9573793ba38d501250df96d88b4
gabisgeorge <gabisov.georgy@gmail.com>
2026-06-09 11:56:39 +0300

    Создана первая форма

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SessionExamsForm
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBoxAll = new System.Windows.Forms.ListBox();
+            this.listBoxCalendar = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBoxAll);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 450);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Все предметы";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBoxCalendar);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox2.Location = new System.Drawing.Point(600, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 450);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Расписание экзаменов";
+            // 
+            // listBoxAll
+            // 
+            this.listBoxAll.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxAll.FormattingEnabled = true;
+            this.listBoxAll.Location = new System.Drawing.Point(3, 16);
+            this.listBoxAll.Name = "listBoxAll";
+            this.listBoxAll.Size = new System.Drawing.Size(194, 431);
+            this.listBoxAll.TabIndex = 1;
+            // 
+            // listBoxCalendar
+            // 
+            this.listBoxCalendar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxCalendar.FormattingEnabled = true;
+            this.listBoxCalendar.Location = new System.Drawing.Point(3, 16);
+            this.listBoxCalendar.Name = "listBoxCalendar";
+            this.listBoxCalendar.Size = new System.Drawing.Size(194, 431);
+            this.listBoxCalendar.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Расписание";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBoxAll;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBoxCalendar;
     }
 }
 
