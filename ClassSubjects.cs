bf567d67f776776db385dd103e1f1ec3d82d3c6b
gabisgeorge <gabisov.georgy@gmail.com>
2026-06-09 11:50:52 +0300

    Добавлен класс ClassSubjects

---
+++ ClassSubjects.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionExamsForm
+{
+    internal class ClassSubjects
+    {
+        public string Subject { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public ClassSubjects(string subject)
+        {
+            Subject = subject;
+        }
+        public ClassSubjects(string subject, DateTime dateTime)
+        {
+            Subject = subject;
+            DateTime = dateTime;
+        }
+        public string SubjectData()
+        {
+            return Subject + " " + DateTime.ToString();
+        }
+    }
+}
